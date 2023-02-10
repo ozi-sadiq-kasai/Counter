5710f6c629764299c868c0703c6577d1794a35cc
ozi <ozisadiqkasai@gmail.com>
2023-02-10 20:34:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let display = document.querySelector(".counter-num")
+
+
+let count = 0;
+function increase(num){
+ count += num;
+ display.innerHTML=count
+}
+
+function decrease(num){
+  count += num;
+  display.innerHTML=count
+ }
+
+
+
+
+
+
+
